de2b0e066e89118bfdc89a2d0a4f30a747c8884d
jwl1989 <jwl1989@qq.com>
2020-01-05 16:47:02 +0800

    update config_json

--- config_json/test.py
+++ config_json/test.py
@@ -1,11 +1,26 @@
 #coding:utf-8
 import json
-
-def get_config_file():
-	file = "G:\\python\\config.json" 
-	data = json.load(open(file))
-	for key in data.keys():
-		print('key: %s value: %s' % (key,data.get(key)))
+class icc():
+	def __init__(self):
+		print("init...")
+	def get_config_file(self):
+		try:
+			file = "G:\\python\\config.json" 
+			self.config_data = json.load(open(file))
+			for key in config_data.keys():
+				print('key: %s value: %s' % (key,config_data.get(key)))
+			return self.config_data
+		except Exception as e:
+			print("ERROR is ",e)
+			return False
+			
+	def start_app():
+		if "sd" in self.config_data.keys():
+			print("deploy app in sd")
+			for app in self.config_data['sd']['app']
+				
+		if "dp" in self.config_data.keys():
+			print("deploy app in dp")
 		
 if __name__ == '__main__':
 	get_config_file()
